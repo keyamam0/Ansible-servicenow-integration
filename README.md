2306b9e6bb20cc50649f256e4a9b3037dee0a1b1
keyamamo <keyamamo>
2024-06-16 11:23:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ansible&Terraform インテグレーションSample Part2
+このサンプルでは、AnsibleからTerraformスクリプトとAnsibleプレイブックを統合して実行する方法を示しています
---
+++ terraform/main.tf
@@ -0,0 +1,66 @@
+# Backend Set
+terraform {
+  backend "s3" {
+  }
+}
+
+# Variables
+variable "keypair_name"{
+}
+variable "TF_VAR_tag"{
+}
+variable "TF_VAR_expire"{
+}
+variable "subnet_id"{
+}
+variable "a_zone"{
+}
+
+# AWS Provider Config
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+# EC2 Create
+resource "aws_instance" "tf-ec2-A" {
+  ami = "ami-01b32aa8589df6208"
+  instance_type = "t2.micro"
+  key_name = var.keypair_name
+  availability_zone = var.a_zone
+  subnet_id = var.subnet_id
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Server-A-${var.TF_VAR_tag}"
+    myGroup = var.TF_VAR_tag
+    expire = var.TF_VAR_expire
+  }
+}
+
+resource "aws_instance" "tf-ec2-B" {
+  ami = "ami-01b32aa8589df6208"
+  instance_type = "t2.micro"
+  key_name = var.keypair_name
+  availability_zone = var.a_zone
+  subnet_id = var.subnet_id
+  associate_public_ip_address = true 
+
+  tags = {
+    Name = "Server-B-${var.TF_VAR_tag}"
+    myGroup = var.TF_VAR_tag
+    expire = var.TF_VAR_expire
+  }
+}
+
+# EBS Create
+resource "aws_ebs_volume" "tf-ebs-01" {
+  availability_zone = var.a_zone
+  size              = 50
+}
+
+# EC2インスタンスへのEBSボリュームのアタッチ
+resource "aws_volume_attachment" "example_attachment" {
+  device_name = "/dev/sdf"
+  volume_id   = aws_ebs_volume.tf-ebs-01.id
+  instance_id = aws_instance.tf-ec2-A.id
+}
